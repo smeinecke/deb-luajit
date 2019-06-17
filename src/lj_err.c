8be1ad8505680bbaa28fc531b89d28fe09eb1c2d
Kevin Roy <kiniou@gmail.com>
2019-06-17 15:50:49 +0200

    New upstream version 2.1.20190530

--- src/lj_err.c
+++ src/lj_err.c
@@ -150,6 +150,7 @@ static void *err_unwind(lua_State *L, void *stopcf, int errcode)
     case FRAME_CONT:  /* Continuation frame. */
       if (frame_iscont_fficb(frame))
 	goto unwind_c;
+      /* fallthrough */
     case FRAME_VARG:  /* Vararg frame. */
       frame = frame_prevd(frame);
       break;
@@ -508,6 +509,7 @@ LJ_NOINLINE void LJ_FASTCALL lj_err_throw(lua_State *L, int errcode)
 {
   global_State *g = G(L);
   lj_trace_abort(g);
+  g->saved_jit_base = g->jit_base;
   setmref(g->jit_base, NULL);
   L->status = LUA_OK;
 #if LJ_UNWIND_EXT
